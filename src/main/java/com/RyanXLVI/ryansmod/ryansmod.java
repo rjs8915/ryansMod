3f9c9177a7aa05ea110b9321aeb02df9b616d4fd
rjs8915 <54720628+rjs8915@users.noreply.github.com>
2019-11-14 20:44:09 -0500

    Add files via upload

---
+++ src/main/java/com/RyanXLVI/ryansmod/ryansmod.java
@@ -0,0 +1,70 @@
+package com.RyanXLVI.ryansmod;
+
+import com.RyanXLVI.ryansmod.blocks.FirstBlock;
+import com.RyanXLVI.ryansmod.blocks.FirstBlockTile;
+import com.RyanXLVI.ryansmod.blocks.ModBlocks;
+import com.RyanXLVI.ryansmod.items.FirstItem;
+import com.RyanXLVI.ryansmod.setup.ClientProxy;
+import com.RyanXLVI.ryansmod.setup.IProxy;
+import com.RyanXLVI.ryansmod.setup.ModSetup;
+import com.RyanXLVI.ryansmod.setup.ServerProxy;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("ryansmod")
+public class ryansmod
+{
+    public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+
+    public static ModSetup setup = new ModSetup();
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ryansmod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        setup.init();
+        proxy.init();
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().register(new FirstBlock());
+        }
+
+        @SubscribeEvent
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+            Item.Properties properties = new Item.Properties()
+                    .group(setup.itemGroup);
+            event.getRegistry().register(new BlockItem(ModBlocks.FIRSTBLOCK,
+                    properties).setRegistryName(
+                    "firstblock"));
+            event.getRegistry().register(new FirstItem());
+        }
+
+        @SubscribeEvent
+        public static void onTileEntityRegistry(final RegistryEvent.Register<TileEntityType<?>> event) {
+            event.getRegistry().register(TileEntityType.Builder.create(FirstBlockTile::new, ModBlocks.FIRSTBLOCK).build(null).setRegistryName("firstblock"));
+        }
+    }
+}
